25827884bcc975f3b927795c4007231093d0fd5e
EvanKim <youngchan.kim@edu.sait.ca>
2023-01-21 00:20:57 -0700

    mongoDB connected

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+app.get("/", (req, res) => res.send("API running..."));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
